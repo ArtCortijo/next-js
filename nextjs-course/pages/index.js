d833cef002a5ad7c710f44d86b3c4963fd7ad04d
Arturo cortijo Purizaca <arts084@gmail.com>
2021-10-13 22:01:10 -0400

    section 5 - 90 - 05-prj-data-fetching

--- nextjs-course/pages/index.js
+++ nextjs-course/pages/index.js
@@ -1,9 +1,16 @@
+import path from 'path';
+// The fs (filesystem) imports the file system module from node JS. This is not a third party package
+import fs from 'fs/promises';
+
 function HomePage(props) {
   const { products } = props;
   console.log(products);
+  
   return (
     <ul>
-      {products.map(product => <li key={product.id}>{product.title}</li>)}
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
 }
@@ -15,12 +22,24 @@ function HomePage(props) {
 // Again code that will never be visible on the client site that fetches data, and exposes data through props to this homepage component.
 // the getStaticProps must always return an object with a props key
 export async function getStaticProps() {
-  return { props: {
-    products: [{
-      id: 'p1',
-      title: 'Product 1'
-    }]
-  }};
+  // this path will lead to the dummy backend json file.
+  // tell path join in which path we're starting. Get to the current directory by using node js object or concept -> the process object which is globally available in node js.
+  // The process.cwd() method returns the current working directory (cwd) of the Node.js process.
+  // IMPORTANT: this cwd will not be the pages folder. Instead when the file is executed, next js will be executing it and it will be treat all the files as if they were at the root project folder.
+  // So the current working directory will be the overall project folder instead of the pages folder.
+  // The next argument is data because we wanna dive into the data folder.
+  // Then the file name we wanna use
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  // readFile wants a callback to conintue. It returns a promise
+  const jsonData = await fs.readFile(filePath);
+  // To get the actual data. The parse(jsonData) converts it into regular js object.
+  const data =JSON.parse(jsonData);
+
+  return { 
+    props: {
+      products: data.products
+    }
+  };
 }
 
 export default HomePage;
