c7b1649bd9cbf24c834efe0c362a023df8fcb3f6
Arturo cortijo Purizaca <arts084@gmail.com>
2021-10-22 14:08:35 -0400

    section 6 - 116. Adding Static Site Generation (SSG) On The Home Page

--- nextjs-course/pages/index.js
+++ nextjs-course/pages/index.js
@@ -1,63 +1,24 @@
-import path from 'path';
-// The fs (filesystem) imports the file system module from node JS. This is not a third party package
-import fs from 'fs/promises';
-
-import Link from 'next/link';
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
 
 function HomePage(props) {
-  const { products } = props;
-  
+
   return (
-    <ul>
-      {products.map((product) => (
-        <li key={product.id}>
-          <Link href={`/products/${product.id}`}>{product.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <EventList items={props.events} />
+    </div>
   );
 }
 
-// pre-fetch the data before we create this component (from dummy-backend.json in the data folder) And before this component page gets pre rendered by next JS. 
-// For that we must add the getStaticProps(). ***It must always be named getStaticProps*** 
-// And actually what this function does is it prepares the props for your component. So it will execute getStaticProps first and then the component function (HomePage(props))
-// and therefore in this get static props function. You can run any code you want.
-// Again code that will never be visible on the client site that fetches data, and exposes data through props to this homepage component.
-// the getStaticProps must always return an object with a props key
+// Since it's the main page, we want to have the data ready to go asap
 export async function getStaticProps() {
-    console.log('Re-Generating...');
-    // this path will lead to the dummy backend json file.
-  // tell path join in which path we're starting. Get to the current directory by using node js object or concept -> the process object which is globally available in node js.
-  // The process.cwd() method returns the current working directory (cwd) of the Node.js process.
-  // IMPORTANT: this cwd will not be the pages folder. Instead when the file is executed, next js will be executing it and it will be treat all the files as if they were at the root project folder.
-  // So the current working directory will be the overall project folder instead of the pages folder.
-  // The next argument is data because we wanna dive into the data folder.
-  // Then the file name we wanna use
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-  // readFile wants a callback to conintue. It returns a promise
-  const jsonData = await fs.readFile(filePath);
-  // To get the actual data. The parse(jsonData) converts it into regular js object.
-  const data =JSON.parse(jsonData);
-  
-  if (!data) {
-    return { 
-      redirect: {
-        destination: '/no-data'
-      } 
-    };
-  }
+  const featuredEvents = await getFeaturedEvents();
 
-  if (data.products.length === 0) {
-    return { notFound: true };
-  }
-
-  // revalidate -> next JS goes ahead and pre-renders pages. But it will now also do this Incremental Static Regeneration (ISR). In this example, the starting page (index) should be regenerated every 60 seconds no matter how many loads or reloads within those 60 seconds.
-  return { 
+  return {
     props: {
-      products: data.products
-    },
-    revalidate: 60
-  };
+      events: featuredEvents
+    }
+  }
 }
 
 export default HomePage;
