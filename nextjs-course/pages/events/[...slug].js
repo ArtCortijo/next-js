9b7e825a809d1e8048406c48a44e978346ca1eac
Arturo cortijo Purizaca <arts084@gmail.com>
2021-10-24 12:59:03 -0400

    section 6 - 121. Adding Client-Side Data Fetching

--- nextjs-course/pages/events/[...slug].js
+++ nextjs-course/pages/events/[...slug].js
@@ -1,5 +1,6 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
 
 import { getFilteredEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
@@ -8,20 +9,53 @@ import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/error-alert';
 
 function FilteredEventsPage(props) {
+  // Client side data fetching
+  const [loadedEvents, setLoadedEvents] = useState();
   const router = useRouter();
   const filterData = router.query.slug;
 
-  // if (!filterData) {
-  //   return <p className='center'>Loading...</p>;
-  // }
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR('https://nextjs-course-section6-default-rtdb.firebaseio.com/events.json', fetcher);
 
-  // const filteredYear = filterData[0];
-  // const filteredMonth = filterData[1];
+  useEffect(() => {
+    if (data) {
+      const events = [];
 
-  // const numYear = +filteredYear;
-  // const numMonth = +filteredMonth;
+      for (const key in data) {
+        events.push({
+          id: key, 
+          ...data[key]
+        });
+      }
 
-  if (props.hasError) {
+      setLoadedEvents(events);
+    }
+  }, [data]);
+  
+  if (!loadedEvents) {
+    return <p className='center'>Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return eventDate.getFullYear() === numYear && eventDate.getMonth() === numMonth - 1;
+  });
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12 || 
+    error
+  ) {
     return (
       <Fragment>
         <ErrorAlert>
@@ -33,8 +67,6 @@ function FilteredEventsPage(props) {
       </Fragment>
     );
   }
-
-  const filteredEvents = props.events;
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -49,7 +81,7 @@ function FilteredEventsPage(props) {
     );
   }
 
-  const date = new Date(props.date.year, props.date.month - 1);
+  const date = new Date(numYear, numMonth - 1);
 
   return (
     <Fragment>
@@ -59,52 +91,52 @@ function FilteredEventsPage(props) {
   );
 }
 
-export async function getServerSideProps(context) {
-  // params is the same as router.query.slug
-  const { params } = context;
-  // slug -> our catch-all grouping parameter name (filename)
-  const filterData = params.slug;
-  const filteredYear = filterData[0];
-  const filteredMonth = filterData[1];
+// export async function getServerSideProps(context) {
+//   // params is the same as router.query.slug
+//   const { params } = context;
+//   // slug -> our catch-all grouping parameter name (filename)
+//   const filterData = params.slug;
+//   const filteredYear = filterData[0];
+//   const filteredMonth = filterData[1];
 
-  const numYear = +filteredYear;
-  const numMonth = +filteredMonth;
+//   const numYear = +filteredYear;
+//   const numMonth = +filteredMonth;
 
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2030 ||
-    numYear < 2021 ||
-    numMonth < 1 ||
-    numMonth > 12
-  ) {
-    // can't return jsx. That's not possible in getServerSideProps. We always have to return an object.
-    return {
-      props: { hasError: true }
+//   if (
+//     isNaN(numYear) ||
+//     isNaN(numMonth) ||
+//     numYear > 2030 ||
+//     numYear < 2021 ||
+//     numMonth < 1 ||
+//     numMonth > 12
+//   ) {
+//     // can't return jsx. That's not possible in getServerSideProps. We always have to return an object.
+//     return {
+//       props: { hasError: true }
 
-      // // show message 404 not found
-      // notFound: true,
-      // // we could also redirect to a specific error page
-      // redirect: {
-      //   destination: '/error'
-      // }
-    };
-  }
+//       // // show message 404 not found
+//       // notFound: true,
+//       // // we could also redirect to a specific error page
+//       // redirect: {
+//       //   destination: '/error'
+//       // }
+//     };
+//   }
 
-  const filteredEvents = await getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  });
+//   const filteredEvents = await getFilteredEvents({
+//     year: numYear,
+//     month: numMonth,
+//   });
 
-  return {
-    props: {
-      events: filteredEvents,
-      date: {
-        year: numYear,
-        month: numMonth,
-      },
-    }
-  }
-}
+//   return {
+//     props: {
+//       events: filteredEvents,
+//       date: {
+//         year: numYear,
+//         month: numMonth,
+//       },
+//     }
+//   }
+// }
 
 export default FilteredEventsPage;
